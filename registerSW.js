e2d045f28623a329c610d0626cf8c8916c82e94e
yasunosubaru <yasunosubaru@users.noreply.github.com>
2026-08-30 15:05:28 +0000

    deploy: aaee78434d1758d77b2bea023abd00e8ba0a4570

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ham_web/sw.js', { scope: '/ham_web/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
